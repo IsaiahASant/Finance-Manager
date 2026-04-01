669199e77ee757421e19e105a68d647e2c104526
Sean Powers <s23powe@siena.edu>
2026-04-01 14:44:58 -0400

    Added ExpenseTransaction abstract class

---
+++ ExpenseTransaction.java
@@ -0,0 +1,11 @@
+/**
+ * Abstract class to handle different kinds of transactions
+ * 
+ * @author Sean Powers
+ * @version Spring 2026
+ * 
+ */
+
+public abstract class ExpenseTransaction {
+    
+}
