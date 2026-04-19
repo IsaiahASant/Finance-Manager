8074a9e11310f01059a505e745badef809369c09
Isaiah Santamaria <ia07sant@siena.edu>
2026-04-19 03:24:01 +0000

    adding JContainer for handling positioning of each component, also maded corners of RecentTransactionCont so it can look more clean

--- UserInterface components/ColorTheme.java
+++ UserInterface components/ColorTheme.java
@@ -4,11 +4,17 @@ import java.awt.Color;
  * @version 4/17/2026
  */
 public class ColorTheme {
+
+    /** color theme of the application */
     public final Color PRIMARY = new Color(12,21,25);
     public final Color SECONDARY = new Color(22,33,39);
     public final Color TERTIARY = new Color(58,53,52);
     public final Color QUATERNARY = new Color(114, 75,57);
     public final Color QUINARY = new Color(207,157,123);
+
+    /**Colors used to highlight income/expense in transaction table */
+    public final Color INCOME = new Color(33 , 234 , 48 );
+    public final Color EXPENSE = new Color(235, 44, 22);
 
     public ColorTheme(){
     }
