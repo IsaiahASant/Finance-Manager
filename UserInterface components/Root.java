cc30b4ce80ee3828a06a1c7d50a5de801d7ecfdc
Isaiah Santamaria <ia07sant@siena.edu>
2026-04-20 02:28:42 +0000

    finished up banner,  updated Root for banner testing and adding all other created components, also updated UITester. UITester should not be changed besides its size for adjuistment of size, but otherwise root and other containers can be changed

--- UserInterface components/Root.java
+++ UserInterface components/Root.java
@@ -1,11 +1,43 @@
-import javax.swing.*;
-/**
- * 
- * @Author Isaiah Santamaria
- * @Version 4/16/2026
- */
-public class Root extends JFrame {
 
-    
+import java.awt.BorderLayout;
+import javax.swing.*;
+
+/**
+ * main container of root
+ * @author Isaiah Santamaria
+ * @version 4/16/2025
+ */
+public class Root extends JPanel {
+    public RecentTransaction rTCont;
+    public InputCont inputCont;
+    public AccountsCont AcntCont;
+    public ContentCont content;
+    public static final ColorTheme COLOR_THEME = new ColorTheme();
+    public Root(){
+        setLayout(new BorderLayout());
+        //setBorder(BorderFactory.createEmptyBorder(40, 40, 40, 40)); // makes Root have auto padding
+        setBackground(COLOR_THEME.PRIMARY);
+        setSize(1500,500);
+
+
+        JPanel bottomPanel = new JPanel();
+     
+        add(new Banner(), BorderLayout.NORTH);
+
+        //add(bottomPanel, BorderLayout.CENTER); // stretches to fill remaining space
+
+        
+        
+        
+        
+        
+        setVisible(true);
+        
+    }
+
+    public static void main(String []args){
+        new Root();
+    }
+
     
 }
--- UserInterface components/UITester.java
+++ UserInterface components/UITester.java
@@ -4,12 +4,29 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class UITester implements Runnable {
+public class UITester extends JFrame {
+    public RecentTransaction rTCont;
+    public InputCont inputCont;
+    public AccountsCont AcntCont;
+    public ContentCont content;
 
-    @Override
-    public void run(){
+    public UITester(){
+        setTitle("Personal Finance Tacker by Software security");
+        setSize(1500,500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBackground(new ColorTheme().PRIMARY);
+
+        add(new Root());
+        
+        setVisible(true);
 
     }
+
+    public static void main(String [] args){
+        new UITester();
+    }
+
+    
     
 
 
