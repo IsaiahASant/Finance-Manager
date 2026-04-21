525374408290760d252c90d9428cdf3001e4c024
Isaiah Santamaria <ia07sant@siena.edu>
2026-04-21 04:02:30 +0000

    added actionListener to inputCont so it can take in users information

--- UserInterface components/InputCont.java
+++ UserInterface components/InputCont.java
@@ -1,12 +1,7 @@
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.GridBagLayout;
-import java.awt.RenderingHints;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 
 
 
@@ -19,13 +14,34 @@ import javax.swing.table.DefaultTableModel;
  */
 public class InputCont extends JPanel{
         /** display transaction variables */
-        private JTable table;
-        private DefaultTableModel tableModel;
-        private JButton print_csv_btn;
+        
         /**color Theme Object */
         private static final ColorTheme COLOR_THEME = new ColorTheme();
+
+        /** Buttons components */
         private JButton income_btn;
         private JButton expense_btn;
+        private JButton print_csv_btn;
+        private JButton submit = new JButton("Submit");
+
+
+        /**Amount that is used for income or expense */
+        private JSpinner amount = new JSpinner(new SpinnerNumberModel(1.0,0.01, Double.MAX_VALUE,1));;
+
+        /** text field for reasoning */
+        private JTextField reason = new JTextField(20);
+
+        /** text field for reasoning */
+        private JTextField items = new JTextField(20);
+
+        private ButtonGroup group = new ButtonGroup();
+
+         /**temporary account list */
+        private String [] accountNames = {"Checking","Savings","Joint"};
+
+        /** String */
+        private String [] categoryNames = {"Bills", "Shopping", "Necessities", "Utilities", "Others"};
+
 
     public InputCont(){ 
         setOpaque(false);
@@ -46,8 +62,10 @@ public class InputCont extends JPanel{
 
         /**adding interactive buttons */
         income_btn.addActionListener(new ActionListener() {
+            
             @Override
             public void actionPerformed(ActionEvent e) {
+                displayIncome();
                 System.out.println("Income btn Clicked!");
             }
         });
@@ -55,6 +73,7 @@ public class InputCont extends JPanel{
         expense_btn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                displayExpense();
                 System.out.println("expense btn Clicked!");
             }
         });
@@ -84,23 +103,212 @@ public class InputCont extends JPanel{
     }
     
     /**
-     * displays the income inputs
+     * displays the income inputs, when
+     * submit button is clicked, it goes back to the 
+     * menu options
      */
     private void displayIncome(){
+        clearPanel();
+        setLayout(new GridLayout(6,1));
+        setBorder(BorderFactory.createEmptyBorder(0,20,40,20)); //10 pixels of padding in each side of container
+
+        add(new JLabel("Income Transaction\\"){{
+            setFont(new Font("Brush Script MT", Font.PLAIN, 20));
+            setForeground(COLOR_THEME.QUINARY);
+
+        }});
+
+        add(new JContainer(){{
+            
+            amount.setPreferredSize(new Dimension(100,25));
+            add(new JLabel("Amount: "){{
+                setFont(new Font("Serif", Font.BOLD, 16));
+                setForeground(COLOR_THEME.QUINARY);
+            }});
+            
+
+            
+            add(amount);
+            
+
+            
+        }});
+        /****************Radio section that currently has a problem */
+        add(new JContainer(){{
+            amount.setPreferredSize(new Dimension(100,25));
+            add(new JLabel("Source: "){{
+                setFont(new Font("Serif", Font.BOLD, 16));
+                setForeground(COLOR_THEME.QUINARY);
+            }});
+            //add(amount);
+
+            add(new JComponent(){{
+                setLayout(new BorderLayout());
+                for(String str : categoryNames){
+                    add(new JRadioButton(str));
+                }
+            }});
+            
+            
+        }});
+        /****************Radio section that currently has a problem */
+        add(new JContainer(){{
+            
+            amount.setPreferredSize(new Dimension(100,25));
+            add(new JLabel("Items: "){{
+                setFont(new Font("Serif", Font.BOLD, 16));
+                setForeground(COLOR_THEME.QUINARY);
+            }});
+           
+
+            add(items);
+            
+
+            
+        }});
+        add(new JContainer(){{
+            
+            amount.setPreferredSize(new Dimension(100,25));
+            add(new JLabel("Reason: "){{
+                setFont(new Font("Serif", Font.BOLD, 16));
+                setForeground(COLOR_THEME.QUINARY);
+            }});
+            
+            add(reason);
+            
+
+            
+        }});
+
+        add(new JContainer(){{
+            
+            
+            submit.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    displayOptions();
+                    System.out.println("submit income btn Clicked!");
+                }
+            });
+            
+            add(submit);
+               
+        }});
+
+
 
     }
 
     /**
-     * display expense income
+     * display expense transaction inputs
+     * when submit button is clicked, it goes back to menu 
+     * options
      */
     private void displayExpense(){
+        
+        clearPanel();
+        setLayout(new GridLayout(6,1));
+        setBorder(BorderFactory.createEmptyBorder(0,20,40,20)); //10 pixels of padding in each side of container
 
+        add(new JLabel("Expense Transaction\\"){{
+            setFont(new Font("Brush Script MT", Font.PLAIN, 20));
+            setForeground(COLOR_THEME.QUINARY);
+
+        }});
+
+        add(new JContainer(){{
+            
+            amount.setPreferredSize(new Dimension(100,25));
+            add(new JLabel("Amount: "){{
+                setFont(new Font("Serif", Font.BOLD, 16));
+                setForeground(COLOR_THEME.QUINARY);
+            }});
+            
+
+            
+            add(amount);
+            
+
+            
+        }});
+        /****************Radio section that currently has a problem */
+        add(new JContainer(){{
+            amount.setPreferredSize(new Dimension(100,25));
+            add(new JLabel("Category: "){{
+                setFont(new Font("Serif", Font.BOLD, 16));
+                setForeground(COLOR_THEME.QUINARY);
+            }});
+            //add(amount);
+
+            add(new JComponent(){{
+                setLayout(new BorderLayout());
+                for(String str : categoryNames){
+                    add(new JRadioButton(str));
+                }
+            }});
+            
+            
+        }});
+        /****************Radio section that currently has a problem */
+        add(new JContainer(){{
+            
+            amount.setPreferredSize(new Dimension(100,25));
+            add(new JLabel("Items: "){{
+                setFont(new Font("Serif", Font.BOLD, 16));
+                setForeground(COLOR_THEME.QUINARY);
+            }});
+           
+
+            add(items);
+            
+
+            
+        }});
+        add(new JContainer(){{
+            
+            amount.setPreferredSize(new Dimension(100,25));
+            add(new JLabel("Reason: "){{
+                setFont(new Font("Serif", Font.BOLD, 16));
+                setForeground(COLOR_THEME.QUINARY);
+            }});
+            
+            add(reason);
+            
+
+            
+        }});
+
+        add(new JContainer(){{
+            
+            
+            submit.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    displayOptions();
+                    System.out.println("submit expense btn Clicked!");
+                }
+            });
+            
+            add(submit);
+               
+        }});
+ 
+    }
+
+    /**
+     * clears main panel
+     */
+    private void clearPanel(){
+        removeAll(); //clears all child component
+        revalidate(); //tells the layout manager to re-layout
+        repaint(); //repaint the panel
     }
 
     /**
      * display options inputs
      */
     private void displayOptions(){
+        clearPanel();
         setLayout(new GridBagLayout());
 
         add(new JContainer(){{
@@ -110,5 +318,8 @@ public class InputCont extends JPanel{
         
 
     }
+
+
+    
 
 }
