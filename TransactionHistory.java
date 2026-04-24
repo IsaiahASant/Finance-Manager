1cae682d28e4e7227f32e36788a48582df687bdf
Sean Powers <s23powe@siena.edu>
2026-04-23 20:38:30 -0400

    constructor and instance variabels for TransactionHistory

--- TransactionHistory.java
+++ TransactionHistory.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+
+
 public class TransactionHistory {
+    //instance variables for incomes and expenses
+    private ArrayList<Integer> incomes;
+    private ArrayList<Integer> expenses;
+
+    public void TransactionHistory(ArrayList<Integer> income, ArrayList<Integer> expense){
+        incomes = income;
+        expenses = expense;
+    }
+
     
+
 }
