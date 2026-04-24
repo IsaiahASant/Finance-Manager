a21972fdb15986f6fd79267d9a4ea6f2e958836a
Sean Powers <s23powe@siena.edu>
2026-04-23 20:41:30 -0400

    Did string methods

--- TransactionHistory.java
+++ TransactionHistory.java
@@ -3,14 +3,26 @@ import java.util.ArrayList;
 
 public class TransactionHistory {
     //instance variables for incomes and expenses
-    private ArrayList<Integer> incomes;
+    private ArrayList<Integer> income;
     private ArrayList<Integer> expenses;
 
     public void TransactionHistory(ArrayList<Integer> income, ArrayList<Integer> expense){
-        incomes = income;
+        this.income = income;
         expenses = expense;
     }
 
-    
+    public String getIncomeTransactions(){
+        return income.toString();
+    }
+
+    public String getExpenseTransactions(){
+        return expenses.toString();
+    }
+
+    @Override 
+    public String toString(){
+        return "Income: " + getIncomeTransactions() + "\nExpenses: " + getExpenseTransactions();
+    }
+
 
 }
