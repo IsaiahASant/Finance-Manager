d43d22aaf14ce479288a897c2da359a1d5ebf170
Sean Powers <s23powe@siena.edu>
2026-04-27 08:32:53 -0400

    Testing SQL, but it doesn't seem to work yet

--- backend/Transactions.sql
+++ backend/Transactions.sql
@@ -1,1 +1,4 @@
-CREATE TABlE TRANSACTIONS  (transactionType NOT NULL, amount NOT NULL, reason NOT NULL, id NOT NULL, source NOT NULL); 
+CREATE TABLE TRANSACTIONS  (transactionType NOT NULL, amount NOT NULL, reason NOT NULL, id NOT NULL, source NOT NULL); 
+
+INSERT INTO TRANSACTIONS (transactionType, amount, reason, id, source)
+VALUES("Grocery", "500", "Needed a lot of food", "5", "person");
