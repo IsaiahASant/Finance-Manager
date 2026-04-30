efb92a23cc5477b4e16a392dc1ee0c17c87b25b0
Sean Powers <s23powe@siena.edu>
2026-04-30 16:01:18 -0400

    backend of savings

--- components/AccountsChart.java
+++ components/AccountsChart.java
@@ -13,6 +13,15 @@ import javax.swing.*;
 
 public class AccountsChart extends JPanel{
 
+    //will have to input histories from CSV file
+
+
+    //amount to display, will have to go back;
+    private double checkingBalance;
+    private double savingsBalance; 
+    private double jointBalance;
+
+
     public AccountsChart(){
         setPreferredSize(new Dimension(300, 300));
         setBackground(Color.white);
